e26417f6e9c8d533769b6232c251802b5150a3a2
Pool <pool_1541@hotmail.com>
2023-11-09 09:45:35 -0500

    Updated to use theme variants for consistent styling

--- src/components/landing/SearchBarContainer.jsx
+++ src/components/landing/SearchBarContainer.jsx
@@ -1,58 +1,53 @@
-import SearchBar from "@/components/searchBar/SearchBar";
-import { Box, Container, Typography } from "@mui/material";
-import { useTheme } from "@mui/system";
+import SearchBar from '@/components/searchBar/SearchBar';
+import { Box, Container, Typography } from '@mui/material';
+import { useTheme } from '@mui/system';
 
-export default function SearchBarContainer(){
-    const theme = useTheme();
+export default function SearchBarContainer() {
+  const theme = useTheme();
 
-    return (
-        <Box sx={{
-            backgroundImage: `url("../../src/assets/images/landing/imagen_hero.png")`,
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-            height: '488px'
-        }}>
-            <Container sx={{ px:'1rem' }}>
-                <SearchBar/>
-                <Typography
-                    variant="h2" 
-                    color={theme.palette.text.primary}
-                    sx={{
-                        fontFamily: 'Lato',
-                        fontSize: '18px',
-                        fontWeight: 600,
-                        lineHeight: '24px',
-                        letterSpacing: '0px',
-                        textAlign: 'left',
-                        color: '#FDFDFE',
-                        paddingY: '1rem'
-                    }}
-                >
-                    FINANCIAMIENTO SOSTENIBLE
-                </Typography>
+  return (
+    <Box
+      sx={{
+        backgroundImage: `url("../../src/assets/images/landing/imagen_hero.png")`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        height: '488px',
+      }}
+    >
+      <Container sx={{ py: '1.5rem' }}>
+        <SearchBar />
+        <Typography
+          variant='h3'
+          component='h2'
+          color={theme.palette.text.primary}
+          sx={{
+            color: '#FDFDFE',
+            padding: '2.5rem 0 1rem 0',
+          }}
+        >
+          FINANCIAMIENTO SOSTENIBLE
+        </Typography>
 
-                <Box sx={{
-                    width: '70%',
-                    float: 'left',
-                    clear: 'both' 
-                }}>
-                    <Typography
-                        variant="body1" 
-                        sx={{
-                            fontFamily: 'Lato',
-                            fontSize: '28px',
-                            fontWeight: 500,
-                            lineHeight: '33px',
-                            letterSpacing: '0px',
-                            textAlign: 'left',
-                            color: '#FDFDFE', 
-                        }}
-                    >
-                        Impulsamos el desarrollo de finanzas de impacto, liderando la transición hacia un modelo financiero sostenible
-                    </Typography>
-                </Box>
-            </Container>
+        <Box
+          sx={{
+            width: '65%',
+            minWidth: '235px',
+            float: 'left',
+            clear: 'both',
+          }}
+        >
+          <Typography
+            variant='h1'
+            sx={{
+              color: '#FDFDFE',
+            }}
+          >
+            Impulsamos el desarrollo de finanzas de impacto, liderando la transición hacia un modelo
+            financiero sostenible
+          </Typography>
         </Box>
-    )
+      </Container>
+    </Box>
+  );
 }
