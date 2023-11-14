425e266d7c903fd8d5ffaea1ec0d39d54ff31225
Pool <pool_1541@hotmail.com>
2023-11-13 20:45:20 -0500

    Added the 'Categories' section to LandingPage

--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -2,13 +2,15 @@ import { Container } from '@mui/material';
 import Goals from '@/pages/landing/components/Goals';
 import Posts from '@/pages/landing/components/Posts';
 import SearchBarContainer from '@/pages/landing/components/SearchBarContainer';
+import Categories from '@/pages/landing/components/Categories';
 
 function LandingPage() {
   return (
     <>
       <SearchBarContainer />
-      <Container>
+      <Container sx={{ py: '2.5rem' }}>
         <Goals />
+        <Categories />
         <Posts />
       </Container>
     </>
