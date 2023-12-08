bfd3b51c8c8ba409149a549354d6eb3a359351bd
pool_1541 <pool_1541@hotmail.com>
2023-12-08 12:31:49 -0500

    update LoadMicroentrepreneurshipPage

--- src/pages/admin-microentrepreneurship/pages/load/LoadMicroentrepreneurship.jsx
+++ src/pages/admin-microentrepreneurship/pages/load/LoadMicroentrepreneurship.jsx
@@ -1,3 +1,27 @@
+import LoadForm from '@/pages/admin-microentrepreneurship/pages/load/components/LoadForm';
+import { Box, Container, Grid, Typography } from '@mui/material';
+
 export default function LoadMicroentrepreneurship() {
-  return <div>LoadMicroentrepreneurship</div>;
+  return (
+    <Container sx={{ py: '2.5rem' }}>
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography variant='h1' align='center'>
+            Carga de Microemprendimiento
+          </Typography>
+          <Box>
+            <Typography
+              variant='h2'
+              sx={{ fontSize: '1.25rem', fontWeight: '400' }}
+              align='center'
+              mt={'2rem'}
+            >
+              Completá el formulario para cargar un Microemprendimiento
+            </Typography>
+            <LoadForm />
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
