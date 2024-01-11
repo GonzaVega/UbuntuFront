699dce4d271ed03c82f392b5fd8e364cc28ec02a
pool_1541 <pool_1541@hotmail.com>
2024-01-11 09:50:00 -0500

    update AuthProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from '@emotion/react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
-import { useAuth } from '@/contexts/AuthContext';
+import { AuthProvider } from '@/contexts/AuthContext';
 import { ADMIN_ROUTES, USER_ROUTES } from '@/constants/routes';
 import Login from '@/auth/Login';
 import theme from '@/theme/theme';
@@ -14,36 +14,42 @@ import VisitorMicroentrepreneurshipRouter from '@/pages/microemprendimientos/Vis
 import MicroentrepreneurshipRouter from '@/pages/admin-microentrepreneurship/MicroentrepreneurshipRouter';
 import PostRouter from '@/pages/admin-post/PostRouter';
 import RequestRouter from '@/pages/admin-request/RequestRouter';
+import AuthGuard from '@/guards/AuthGuard';
+import UnAuthGuard from '@/guards/UnAuthGuard';
 
 function App() {
-  const { isAuthenticated } = useAuth();
-
   return (
     <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Router>
-          <NavBar />
-          <Routes>
-            <Route path={USER_ROUTES.HOME} element={<LandingPage />} />
-            <Route path={USER_ROUTES.POSTS} element={<Publications />} />
-            <Route path={USER_ROUTES.SEARCH} element={<SearchPage />} />
-            <Route
-              path={USER_ROUTES.MICROENTREPRENEURSHIPS.ROUTER_ROUTE + '/*'}
-              element={<VisitorMicroentrepreneurshipRouter />}
-            />
-            <Route path={ADMIN_ROUTES.DASHBOARD} element={<Dashboard />} />
-            <Route
-              path={ADMIN_ROUTES.MICROENTREPRENEURSHIPS.ROUTER_ROUTE + '/*'}
-              element={<MicroentrepreneurshipRouter />}
-            />
-            <Route
-              path={ADMIN_ROUTES.CONTACT_REQUESTS.ROUTER_ROUTE + '/*'}
-              element={<RequestRouter />}
-            />
-            {!isAuthenticated && <Route path={USER_ROUTES.LOGIN} element={<Login />} />}
-            <Route path={ADMIN_ROUTES.POSTS.ROUTER_ROUTE + '/*'} element={<PostRouter />} />
-          </Routes>
+          <AuthProvider>
+            <NavBar />
+            <Routes>
+              <Route path={USER_ROUTES.HOME} element={<LandingPage />} />
+              <Route path={USER_ROUTES.POSTS} element={<Publications />} />
+              <Route path={USER_ROUTES.SEARCH} element={<SearchPage />} />
+              <Route
+                path={USER_ROUTES.MICROENTREPRENEURSHIPS.ROUTER_ROUTE + '/*'}
+                element={<VisitorMicroentrepreneurshipRouter />}
+              />
+              <Route element={<UnAuthGuard />}>
+                <Route path={USER_ROUTES.LOGIN} element={<Login />} />
+              </Route>
+              <Route element={<AuthGuard />}>
+                <Route path={ADMIN_ROUTES.DASHBOARD} element={<Dashboard />} />
+                <Route
+                  path={ADMIN_ROUTES.MICROENTREPRENEURSHIPS.ROUTER_ROUTE + '/*'}
+                  element={<MicroentrepreneurshipRouter />}
+                />
+                <Route
+                  path={ADMIN_ROUTES.CONTACT_REQUESTS.ROUTER_ROUTE + '/*'}
+                  element={<RequestRouter />}
+                />
+                <Route path={ADMIN_ROUTES.POSTS.ROUTER_ROUTE + '/*'} element={<PostRouter />} />
+              </Route>
+            </Routes>
+          </AuthProvider>
         </Router>
       </ThemeProvider>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App.jsx';
 import '@/styles/index.css';
-import { AuthProvider } from '@/contexts/AuthContext';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { googleOAuthCredential } from '@/helpers/googleCredentials';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={googleOAuthCredential}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <App />
     </GoogleOAuthProvider>
   </React.StrictMode>,
 );
